493ceda219dffe47b12d03fa1e76c80e4997f077
DimaBohdan <bohdan777.dmytro@gmail.com>
2024-09-28 08:54:12 +0300

    separate input_handler to another file

--- matrix.py
+++ matrix.py
@@ -1,29 +1,5 @@
 from copy import deepcopy
 from square_matrix import *
-def fill_matrix(row_num):
-   matrix_list = []
-   for num in range(row_num):
-      row_list = [float(elem) for elem in input('Input row: ').split(" ")]
-      matrix_list.append(row_list)
-   return matrix_list
-
-def fill(str):
-   a = str.split(']')
-   list = []
-   list.append([i for i in a if i != ''])
-   for i in list[0]:
-      if ',[' in i:
-         b = ',['
-   c = []
-   c.append(list[0][0].replace('[[', ""))
-   l = [i.replace(b, '') for i in list[0][1:]]
-   for i in l:
-      c.append(i)
-   new_lis = [i.split(",") for i in c]
-   new = []
-   for k in new_lis:
-      new.append([int(i) for i in k])
-   return new
 
 class Matrix:
    def __init__(self, raw_matrix: list[list[float]]):
