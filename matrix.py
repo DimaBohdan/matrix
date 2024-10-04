0da1a930a1e52a869aaddf16666766d3f7bdf657
DimaBohdan <bohdan777.dmytro@gmail.com>
2024-10-04 17:21:23 +0300

    test_init created

--- matrix.py
+++ matrix.py
@@ -92,6 +92,7 @@ Enter number of columns: 4
 8 1 3 2
 '''
 if __name__ == "__main__":
+   print(Matrix([[1], [2]]).__dict__)
    m = int(input("Enter number of rows: "))
    #columns_number = int(input("Enter number of columns: "))
    #a = Matrix(input_handler.space_separated_row_by_row(m))
--- tests/test_init_matrix.py
+++ tests/test_init_matrix.py
@@ -1,1 +1,9 @@
+import pytest
+from matrix import Matrix
 
+@pytest.mark.parametrize("raw_matrix, attribute, result", [
+    ([[2, 1, 7], [4, 1, 9], [4, 5, 6]], "raw_matrix", [[2, 1, 7], [4, 1, 9], [4, 5, 6]])
+])
+def test_initialization(raw_matrix, attribute, result):
+    assert Matrix(raw_matrix).__dict__[attribute] == result
+    
