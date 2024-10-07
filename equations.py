570fa9ef5a8ce5d7b1588196dd8de4faa2d2822b
DimaBohdan <bohdan777.dmytro@gmail.com>
2024-10-07 16:11:02 +0300

    equations refactored

--- equations.py
+++ equations.py
@@ -1,16 +1,25 @@
 import input_handler
 
-input_command = int(input("Enter the type of matrix equation (1 or 2):\n 1 - Ax=B\n 2 - xA=B\n"))
-row_a = int(input("Input number of rows for matrix A: "))
-a = input_handler.SquareMatrix(input_handler.space_separated_row_by_row(row_a))
-row_b = int(input("Input number of rows for matrix B: "))
-b = input_handler.SquareMatrix(input_handler.space_separated_row_by_row(row_b))
 
-if input_command == 1:
+def equation_solver():
+    input_command = int(input("Enter the type of matrix equation (1 or 2):\n 1 - Ax=B\n 2 - xA=B\n"))
+    row_a = int(input("Input number of rows for matrix A: "))
+    a = input_handler.SquareMatrix(input_handler.space_separated_row_by_row(row_a))
+    row_b = int(input("Input number of rows for matrix B: "))
+    b = input_handler.Matrix(input_handler.space_separated_row_by_row(row_b))
+    if input_command == 1:
+        first_type_equation(a, b)
+    elif input_command ==2:
+        second_type_equation(a, b)
+    else:
+        print('Input is not correct')
+
+def first_type_equation(a, b):
     res = a.inverse_matrix * b
     print(res)
-elif input_command == 2:
+
+def second_type_equation(a, b):
     res = b * a.inverse_matrix
     print(res)
-else:
-    print('Input is not correct')
+
+equation_solver()
